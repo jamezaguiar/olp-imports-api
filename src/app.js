81c7eed4e96b2ecf83b83ea7e320f0d72e3d121f
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-27 17:11:06 -0300

    Data for fill db

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 
-const Phone = require('./app/schemas/Phone');
-
 const routes = require('./routes');
 
 require('dotenv/config');
